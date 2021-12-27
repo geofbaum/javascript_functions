225abf501ebfb8a3c3a9902d7180d5b2a91b931d
Geof Baum <baum.geoff@gmail.com>
2021-12-27 13:57:44 -0500

    Create compareTwoStrings.js

---
+++ compareTwoStrings.js
@@ -0,0 +1,7 @@
+// see if the letters in second string are all present in the first string
+let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
+  }
+  return true;
